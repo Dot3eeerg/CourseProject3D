2ceece04b5ca7a8a6f3933a5d2b418e431217834
Dot3eeerg <91532031+Dot3eeerg@users.noreply.github.com>
2023-06-04 15:46:04 +0700

    Сделал сборку глобальной матрицы и вектора

--- FEM.cs
+++ FEM.cs
@@ -22,7 +22,7 @@ public class FEM
         _grid = grid;
         _timeGrid = timeGrid;
         _basis = new TriQuadraticBasis();
-        _integration = new Integration(new List<QuadratureNode>());
+        _integration = new Integration(Quadratures.SegmentGaussOrder9());
         _stiffnessMatrix = new(_basis.Size);
         _massMatrix = new(_basis.Size);
         _localVector = new(_basis.Size);
@@ -62,11 +62,96 @@ public class FEM
 
         for (int ielem = 0; ielem < _grid.Elements.Length; ielem++)
         {
-            AssemblyLocalElement(ielem);
+            AssemblyLocalElement(ielem, itime);
+
+            double t01 = _timeGrid[itime] - _timeGrid[itime - 1];
+            double t02 = _timeGrid[itime] - _timeGrid[itime - 2];
+            double t03 = _timeGrid[itime] - _timeGrid[itime - 3];
+
+            _stiffnessMatrix += (1 / t03 + 1 / t02 + 1 / t01) * _massMatrix;
+
+            for (int i = 0; i < _basis.Size; i++)
+            {
+                for (int j = 0; j < _basis.Size; j++)
+                {
+                    AddElement(_grid.Elements[ielem][i], _grid.Elements[ielem][j], _stiffnessMatrix[i, j]);
+                }
+            }
+            
+            AssemblyGlobalVector(ielem, itime);
+            
+            _stiffnessMatrix.Clear();
+            _massMatrix.Clear();
+            _localVector.Fill(0);
         }
     }
 
-    private void AssemblyLocalElement(int ielem)
+    private void AssemblyGlobalVector(int ielem, int itime)
+    {
+        double t01 = _timeGrid[itime] - _timeGrid[itime - 1];
+        double t02 = _timeGrid[itime] - _timeGrid[itime - 2];
+        double t03 = _timeGrid[itime] - _timeGrid[itime - 3];
+        double t12 = _timeGrid[itime - 1] - _timeGrid[itime - 2];
+        double t13 = _timeGrid[itime - 1] - _timeGrid[itime - 3];
+        double t23 = _timeGrid[itime - 2] - _timeGrid[itime - 3];
+
+        double[] qj1 = new double[_basis.Size];
+        double[] qj2 = new double[_basis.Size];
+        double[] qj3 = new double[_basis.Size];
+
+        for (int i = 0; i < _basis.Size; i++)
+        {
+            for (int j = 0; j < _basis.Size; j++)
+            {
+                qj1[i] += _massMatrix[i, j] * _layers[2][_grid.Elements[ielem][j]];
+                qj2[i] += _massMatrix[i, j] * _layers[1][_grid.Elements[ielem][j]];
+                qj3[i] += _massMatrix[i, j] * _layers[0][_grid.Elements[ielem][j]];
+            }
+        }
+
+        for (int i = 0; i < _basis.Size; i++)
+        {
+            _localVector[i] += qj1[i] * t03 * t02 / (t13 * t12 * t01) - qj2[i] * t03 * t01 / (t23 * t12 * t02) +
+                               qj3[i] * t02 * t01 / (t23 * t13 * t03);
+
+            _globalVector[_grid.Elements[ielem][i]] += _localVector[i];
+        }
+    }
+
+    private void AddElement(int i, int j, double value)
+    {
+        if (i == j)
+        {
+            _globalMatrix.Di[i] = value;
+            return;
+        }
+
+        if (i > j)
+        {
+            for (int icol = _globalMatrix.Ig[i]; icol < _globalMatrix.Ig[i + 1]; icol++)
+            {
+                if (_globalMatrix.Jg[icol] == j)
+                {
+                    _globalMatrix.Ggl[icol] += value;
+                    return;
+                }
+            }
+        }
+
+        else
+        {
+            for (int icol = _globalMatrix.Ig[j]; icol < _globalMatrix.Ig[j + 1]; icol++)
+            {
+                if (_globalMatrix.Jg[icol] == i)
+                {
+                    _globalMatrix.Ggu[icol] += value;
+                    return;
+                }
+            }
+        }
+    }
+
+    private void AssemblyLocalElement(int ielem, int itime)
     {
         double hx = _grid.Nodes[_grid.Elements[ielem][26]].X - _grid.Nodes[_grid.Elements[ielem][0]].X;
         double hy = _grid.Nodes[_grid.Elements[ielem][26]].Y - _grid.Nodes[_grid.Elements[ielem][0]].Y;
@@ -78,7 +163,7 @@ public class FEM
             {
                 Func<Point3D, double> kek;
 
-                    int ik = i;
+                int ik = i;
                 int jk = j;
                 kek = point =>
                 {
@@ -88,13 +173,13 @@ public class FEM
                     return psi1 * psi2;
                 };
 
-                _massMatrix[i, j] = hx * hy * hz * _grid.Sigma * _integration.Gauss3D(kek);
+                _massMatrix[i, j] = hx * hy * hz * _integration.Gauss3D(kek);
 
 
                 kek = point =>
                 {
                     double dPsi1 = _basis.GetDPsi(ik, 0, point);
-                    double dPsi2 = _basis.GetDPsi(ik, 0, point);
+                    double dPsi2 = _basis.GetDPsi(jk, 0, point);
 
                     return dPsi1 * dPsi2;
                 };
@@ -103,7 +188,7 @@ public class FEM
                 kek = point =>
                 {
                     double dPsi1 = _basis.GetDPsi(ik, 1, point);
-                    double dPsi2 = _basis.GetDPsi(ik, 1, point);
+                    double dPsi2 = _basis.GetDPsi(jk, 1, point);
 
                     return dPsi1 * dPsi2;
                 };
@@ -112,13 +197,19 @@ public class FEM
                 kek = point =>
                 {
                     double dPsi1 = _basis.GetDPsi(ik, 2, point);
-                    double dPsi2 = _basis.GetDPsi(ik, 2, point);
+                    double dPsi2 = _basis.GetDPsi(jk, 2, point);
 
                     return dPsi1 * dPsi2;
                 };
                 _stiffnessMatrix[i, j] += hy * hx / hz * _grid.Lambda * _integration.Gauss3D(kek);
             }
+
+            _localVector[i] = _test.F(_grid.Nodes[_grid.Elements[ielem][i]], _timeGrid[itime]);
         }
+
+        _localVector = _massMatrix * _localVector;
+
+        _massMatrix = _grid.Sigma * _massMatrix;
     }
     
     private void BuildPortrait()
